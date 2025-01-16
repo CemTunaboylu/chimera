59f87f6b49f93e0197287ac43a5b1296f4ef4cdc
Cem Tunaboylu <cemtunaboylu9@gmail.com>
2025-01-16 23:05:33 +0300

    add token trie matches

--- src/trie.rs
+++ src/trie.rs
@@ -1,9 +1,41 @@
-use trie_rs::map::{Trie, TrieBuilder};
+use trie_rs::{
+    inc_search::Answer,
+    map::{Trie, TrieBuilder},
+};
 
 pub struct TokenTrie<'t, T> {
-    // trie: Trie<&'t Chars<'t> , usize>,
     trie: Trie<char, usize>,
     values: Box<[&'t T]>,
+}
+
+impl<'t, T> TokenTrie<'t, T> {
+    pub fn matches(&self, k: &'t str) -> Result<&T, usize> {
+        let chars: Vec<char> = k.chars().into_iter().collect();
+        let mut search = self.trie.inc_search();
+        let query_result = search.query_until(chars);
+
+        let answer = match query_result {
+            Err(first_unmatch) => return Result::Err(first_unmatch),
+            Ok(i) => i,
+        };
+
+        let prefix_length = search.prefix_len();
+
+        let result = match answer {
+            Answer::Prefix => Result::Err(prefix_length),
+            Answer::Match => {
+                let index = search.value().unwrap();
+                Result::Ok(
+                    self.values
+                        .get(*index)
+                        .map(|opt_ref_ref| *opt_ref_ref)
+                        .unwrap(),
+                )
+            }
+            Answer::PrefixAndMatch => Result::Err(prefix_length),
+        };
+        return result;
+    }
 }
 
 pub struct TokenTrieBuilder<'t, T> {
@@ -25,7 +57,6 @@ impl<'t, T> TokenTrieBuilder<'t, T> {
         let chars: Vec<char> = key.chars().into_iter().collect();
         self.trie_builder.insert(chars, index_to_push);
     }
-
     pub fn build(self) -> TokenTrie<'t, T> {
         TokenTrie {
             trie: self.trie_builder.build(),
@@ -38,7 +69,15 @@ impl<'t, T> TokenTrieBuilder<'t, T> {
 mod tests {
     use super::*;
     enum Call<'t> {
-        Insert { to_insert: &'t [&'t str] },
+        Insert {
+            to_insert: &'t [&'t str],
+        },
+        Matches {
+            to_insert_first: &'t [&'t str],
+            disable_default_check: bool,
+            // usize because each insertion is tested by default
+            expected_matches_results: &'t [(&'t str, usize)],
+        },
     }
 
     macro_rules! trie_tests {
@@ -62,6 +101,27 @@ mod tests {
                         assert_eq!(x, *r.unwrap());
                     }
                 },
+                super::tests::Call::Matches{to_insert_first,disable_default_check, expected_matches_results}  => {
+                    for i in to_insert_first {
+                        ttb.insert(i, &true);
+                    }
+                    let trie = ttb.build();
+                    if !disable_default_check {
+                        for i in to_insert_first {
+                            let r = trie.matches(i);
+                            assert_eq!(&true, r.unwrap());
+                        }
+                    }
+                    for (key, expected) in expected_matches_results.into_iter() {
+                        let r = trie.matches(key);
+                        assert_eq!(true, r.is_err());
+                        if let Result::Err(v) = r {
+                            assert_eq!(*expected, v);
+                        } else {
+                            panic!();
+                        }
+                    }
+                },
             };
         }
     )*
@@ -69,12 +129,20 @@ mod tests {
     }
 
     trie_tests! {
-        a: Call::Insert{to_insert: &["a"]},
-        a_ab_abc: Call::Insert{to_insert: &["a", "ab", "abc"]},
-        a_abcd_ab: Call::Insert{to_insert: &["a", "abcd", "ab"]},
-        impl_fn_struct_for: Call::Insert{to_insert: &["impl", "fn", "struct", "for"]},
-        averylongvariablename: Call::Insert{to_insert: &["averylongvariablename"]},
-        i_i16_i32: Call::Insert{to_insert: &["i", "i16", "i32"]},
-        space: Call::Insert{to_insert: &[" "]},
+        insert_a: Call::Insert{to_insert: &["a"]},
+        insert_a_ab_abc: Call::Insert{to_insert: &["a", "ab", "abc"]},
+        insert_a_abcd_ab: Call::Insert{to_insert: &["a", "abcd", "ab"]},
+        insert_impl_fn_struct_for: Call::Insert{to_insert: &["impl", "fn", "struct", "for"]},
+        insert_averylongvariablename: Call::Insert{to_insert: &["averylongvariablename"]},
+        insert_i_i16_i32: Call::Insert{to_insert: &["i", "i16", "i32"]},
+        insert_space: Call::Insert{to_insert: &[" "]},
+        matches_a_1_b_0: Call::Matches {
+            to_insert_first: &["ab", "aa"],
+            disable_default_check: false,
+            expected_matches_results: &[("a", 1), ("b", 0)] },
+        matches_aa_2_aaa_3: Call::Matches {
+            to_insert_first: &["aaab", "aaa", "aa"],
+            disable_default_check: true,
+            expected_matches_results: &[("aa", 2), ("aaa", 3)] },
     }
 }
