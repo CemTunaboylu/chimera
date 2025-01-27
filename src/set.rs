cc837d15d1f6483d0e10ca76fcfc7ae7e767cd34
Cem Tunaboylu <cemtunaboylu9@gmail.com>
2025-01-27 22:37:45 +0300

    Revert "implement a set with integers"
    This reverts commit 79ff88950b765212f2bad837e40d45e517e1e176.

--- src/set.rs
+++
@@ -1,9 +0,0 @@
-pub trait Indexable {
-    fn index(self) -> usize;
-}
-
-pub(crate) trait Set {
-    fn add(&mut self, item: impl Indexable);
-    fn del(&mut self, item: impl Indexable);
-    fn has(self, item: impl Indexable) -> bool;
-}
