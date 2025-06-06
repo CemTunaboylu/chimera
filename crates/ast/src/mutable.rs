f333b300e4317fe73890b0bee2618c2676ddcd30
Cem Tunaboylu <cemtunaboylu9@gmail.com>
2025-06-07 01:07:00 +0300

    refactor and fix mutable

--- crates/ast/src/mutable.rs
+++ crates/ast/src/mutable.rs
@@ -1,21 +1,19 @@
 use syntax::{language::SyntaxNode, syntax_kind::SyntaxKind};
-use thin_vec::ThinVec;
 
 use crate::{
     errors::ASTError,
     expression::Expr,
-    lang_elems::{error_for_node, get_children_in},
+    lang_elems::{children_with_tokens_without_unwanted, error_for_node},
 };
 
+// In Rust‐style syntax, it is only ever seen “&mut T” or “&T” in type hints—there’s no syntax for “mut T” at the type‐level
+// isolation. Instead, “mut” attaches to a pattern or variable binding, not directly to a “bare” type
 #[derive(Clone, Debug, PartialEq)]
-pub struct Mut(SyntaxNode);
+pub struct Mut(pub Box<Expr>);
 
 impl Mut {
-    pub fn get_mut_nodes_from(node: &SyntaxNode) -> ThinVec<SyntaxNode> {
-        get_children_in(node, SyntaxKind::Mut)
-    }
-    pub fn expr(&self) -> Option<Expr> {
-        Expr::try_from(&self.0).ok()
+    pub fn expr(&self) -> &Expr {
+        &self.0
     }
 }
 
@@ -23,8 +21,12 @@ impl TryFrom<&SyntaxNode> for Mut {
     type Error = ASTError;
 
     fn try_from(mut_node: &SyntaxNode) -> Result<Self, Self::Error> {
-        if let Some(child) = mut_node.first_child() {
-            Ok(Self(child))
+        use SyntaxKind::{KwMut, Whitespace};
+        if let Some(child) =
+            children_with_tokens_without_unwanted(mut_node, [KwMut, Whitespace].as_ref()).first()
+        {
+            let expr = Expr::try_from(child)?;
+            Ok(Self(Box::new(expr)))
         } else {
             Err(error_for_node(mut_node, "to have a child"))
         }
